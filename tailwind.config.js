12ff61721fb85ffa425463a2434b1fe85b2c8818
guna <guna.asin06@gmail.com>
2024-12-26 12:12:38 +0530

    auto-blur-3

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,27 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  safelist: [
+    'autoBlur', // Your custom class
+    'blur-animation', // Keyframe animation name
+  ],
   theme: {
-
-    extend: {},
-    fontFamily: {
-      'tsan': ['Noto Serif', 'serif']
-    }
+    extend: {
+      animation: {
+        'blur-animation': 'blur-animation 0.1s infinate'
+         // Extend Tailwind animations
+      },
+      keyframes: {
+        'blur-animation': {
+          '0%': { filter: 'blur(40px)' },
+          '50%, 53%': { filter: 'blur(0px)' },
+          '100%': { filter: 'blur(40px)' },
+        },
+      },
+      fontFamily: {
+        'tsan': ['Noto Serif', 'serif'], // Extend font families
+      },
+    },
   },
   plugins: [],
-}
-
+};
