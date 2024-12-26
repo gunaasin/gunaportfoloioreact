235718953221caf2b683828af7ea90dacb6eb892
guna <guna.asin06@gmail.com>
2024-12-26 12:53:07 +0530

    auto-blur-4

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -12,18 +12,18 @@ export const About = () => {
   return (
     <div className="flex flex-col w-screen items-center justify-center my-40">
       
-        <p className=" autoBlur leading-10 md:text-6xl md:font-semibold sm:text-sm  w-4/5 text-center  tracking-wide  font-tsan project-text-start pg-text-span" >
+        <p className=" autoBlur leading-10 text-2xl md:text-6xl md:font-semibold sm:text  w-4/5 text-center  tracking-wide  font-tsan project-text-start pg-text-span" >
             {/* <TextGenerateEffect words={words} /> */}
             " Harmonizing the artistry of design with the precision of development, my portfolio showcases a dynamic range of skills. From designing intuitive user interfaces to <span className="pg-text-span">developing robust backend</span> systems, I specialize in creating seamless digital experiences. My expertise spans
         </p>
-        <ul className=" list-none md:text-9xl sm:text-2xl uppercase font-semibold w-full flex flex-col justify-center items-center  font-tsan">
-          <li className="project-text-end py-4 relative right-10 autoBlur  ">Development </li>
-          <li className="project-text-start py-4 relative  autoBlur">Backend +</li>
-          <li className="project-text-start py-4 relative left-36 autoBlur">Architecture</li>
-          <li className=" project-text-end py-4 relative right-52 autoBlur">Designing </li>
-          <li className=" project-text-start py-4 relative left-44 autoBlur">And More.</li>
+        <ul className=" list-none text-5xl  md:text-9xl  uppercase font-semibold w-full flex flex-col justify-center items-center  font-tsan">
+          <li className="project-text-end py-4 relative md:right-10  right-1 autoBlur  ">Development </li>
+          <li className="project-text-start py-4 relative md:left-0 left-12 autoBlur">Backend +</li>
+          <li className="project-text-start py-4 relative md:left-36  autoBlur">Architecture</li>
+          <li className=" project-text-end py-4 relative md:right-52 right-12 autoBlur">Designing </li>
+          <li className=" project-text-start py-4 relative md:left-44 left-10 autoBlur">And More.</li>
         </ul>
-        <p className="autoBlur md:text-5xl md:font-semibold sm:text-sm  w-4/5 text-center text-slate-200 tracking-wide leading-10 font-tsan project-text-end pg-text-span" >
+        <p className="autoBlur leading-10 text-2xl md:text-5xl  md:font-semibold  w-4/5 text-center text-slate-200 tracking-wide  font-tsan project-text-end pg-text-span" >
             {/* <TextGenerateEffect words={words} /> */}
             Every project is a testament to my drive for<span className="pg-text-span"> merging innovation with functionality</span>, delivering solutions that captivate visually and excel technically. From crafting immersive web designs to engineering scalable systems, I am committed to shaping <span className="pg-text-span">impactful digital landscapes</span> that resonate with users worldwide."
             {/* Every project is a testament to my commitment to merging innovation with functionality, delivering solutions that are as visually engaging as they are technically sound. Whether it's crafting immersive web designs or engineering scalable systems, I am dedicated to shaping digital landscapes that leave a lasting impact on users worldwide." */}
